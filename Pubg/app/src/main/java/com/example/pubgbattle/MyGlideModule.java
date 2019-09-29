21127fc793f1f478da0235677b986c85b710f34c
ShubhamRaj <shubh1305@hotmail.com>
2019-09-30 02:41:20 +0530

    removed folder

--- Pubg/app/src/main/java/com/example/pubgbattle/MyGlideModule.java
+++
@@ -1,9 +0,0 @@
-package com.example.pubgbattle;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public final class MyGlideModule extends AppGlideModule {
-
-}
